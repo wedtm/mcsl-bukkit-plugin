ab42611b2fae2d2e47540e2a54a979ad557db2f8
unknown <Paul@.(none)>
2011-02-03 10:42:37 -0500

    Reprogrammed from scratch

--- src/com/bukkit/wedtm/mcsl/MCSL.java
+++ src/com/bukkit/wedtm/mcsl/MCSL.java
@@ -1,226 +1,70 @@
 package com.bukkit.wedtm.mcsl;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.logging.Level;
+import java.io.*;
+import java.util.List;
 import java.util.logging.Logger;
-
 import org.bukkit.Server;
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Priority;
-import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginLoader;
-import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * MCSL for Bukkit
- * 
- * @author <Your Name>
- */
-public class MCSL extends JavaPlugin {
-    private final MCSLPlayerListener playerListener = new MCSLPlayerListener(this);
 
-    public HashMap<String, String>   properties     = new HashMap<String, String>();
+public class Mcsl extends JavaPlugin
+{
+	private static final Logger logger = Logger.getLogger("Minecraft");
+	private final McslPlayerListener playerListener = new McslPlayerListener(this);
+	public final String author;
 
-    public final MCSLUpdater         updater        = new MCSLUpdater(this);
+	public Mcsl(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) throws IOException
+	{
+		super(pluginLoader, instance, desc, folder, plugin, cLoader);
 
-    private final Logger             log            = Logger.getLogger("Minecraft");
+		// Compile author list
+		List<String> authors = desc.getAuthors();
+		int alen = authors.size();
+		if (alen == 1)
+		{
+			author = " by " + authors.get(0);
+		}
+		else if (alen > 1)
+		{
+			int i = 0;
+			StringBuilder bldr = new StringBuilder();
+			for (String a : desc.getAuthors())
+			{
+				if (i + 1 == alen)
+				{
+					if (alen > 2) bldr.append(",");
+					bldr.append(" and ");
+				}
+				else if (i++ > 0)
+				{
+					bldr.append(", ");
+				}
+				bldr.append(a);
+			}
+			bldr.insert(0, " by ");
+			author = bldr.toString();
+		}
+		else
+		{
+			author = "";
+		}
+	}
 
-    private final File               config;
+	@SuppressWarnings("LoggerStringConcat")
+	public void onEnable()
+	{
+		getServer().getPluginManager().registerEvent(Event.Type.PLAYER_JOIN, playerListener, Priority.Monitor, this);
+		getServer().getPluginManager().registerEvent(Event.Type.PLAYER_QUIT, playerListener, Priority.Monitor, this);
 
-    private Boolean                  enabled        = true;
+		logger.info(getDescription().getName() + " version " + getDescription().getVersion() + author + " enabled.");
+	}
 
-    public MCSL(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) throws IOException {
-        super(pluginLoader, instance, desc, folder, plugin, cLoader);
-
-        log.info("[MCSL] Performing checks");
-
-        this.config = new File(folder + "/MCSL.properties");
-
-        if (this.config.exists()) {
-            if (!loadProperties()) {
-                log.info("[MCSL] Error reading from MCSL.properties file.");
-                this.enabled = false;
-            }
-            if (!this.properties.containsKey("mcsl-key")) {
-                log.info("[MCSL] mcsl-key is not set in MCSL.properties.");
-                this.enabled = false;
-            }
-
-            if (!this.properties.containsKey("update-interval")) {
-                log.info("[MCSL] update-interval is not set in MCSL.properties.");
-                this.enabled = false;
-            }
-
-            if (!this.properties.containsKey("max-players")) {
-                log.info("[MCSL] max-players is not set in server.properties.");
-                this.enabled = false;
-            }
-        }
-        else {
-            if (!setupProperties(folder)) {
-                log.info("[MCSL] Error trying to create and setup MCSL Properties file.");
-                this.enabled = false;
-            }
-        }
-    }
-
-    @Override
-    public void onEnable() {
-        if (this.enabled == false) {
-            this.setEnabled(false);
-        }
-        else {
-            PluginManager pm = getServer().getPluginManager();
-
-            pm.registerEvent(Event.Type.PLAYER_LOGIN, playerListener, Priority.Monitor, this);
-            pm.registerEvent(Event.Type.PLAYER_QUIT, playerListener, Priority.Monitor, this);
-
-            log.info(this.getDescription().getName() + " version " + this.getDescription().getVersion() + " is enabled!");
-
-            startData(); // First we send off the initial data which contains information about the server and what plugins are running.
-            updater.timedUpdate(); // Start off our timed action which will constantly send updates to MCSL.
-        }
-    }
-
-    @Override
-    public void onDisable() {
-        log.info("[MCSL] Disabled.");
-    }
-
-    public boolean loadProperties() {
-        Scanner serverScanner = null;
-        try {
-            serverScanner = new Scanner(new FileInputStream("server.properties"));
-            while (serverScanner.hasNextLine()) {
-                String line = serverScanner.nextLine();
-                if (line.startsWith("#"))
-                    continue;
-
-                if (line.startsWith("max-players="))
-                    this.properties.put("max-players", line.split("=")[1].trim().toString());
-
-                if (line.startsWith("pvp="))
-                    this.properties.put("pvp", line.split("=")[1].trim());
-
-                if (line.startsWith("hellworld="))
-                    this.properties.put("hellworld", line.split("=")[1].trim());
-
-                if (line.startsWith("spawn-monsters="))
-                    this.properties.put("spawn-monsters", line.split("=")[1].trim());
-
-                if (line.startsWith("spawn-animals="))
-                    this.properties.put("spawn-animals", line.split("=")[1].trim());
-
-                if (line.startsWith("online-mode="))
-                    this.properties.put("online-mode", line.split("=")[1].trim());
-            }
-        }
-        catch (Exception e) {
-            log.info("[MCSL] Error (server.properties) - " + e);
-            return false;
-        }
-        finally {
-            serverScanner.close();
-        }
-
-        Scanner msclScanner = null;
-        try {
-            msclScanner = new Scanner(new FileInputStream("./" + this.config));
-            while (msclScanner.hasNextLine()) {
-                String line = msclScanner.nextLine();
-                String[] split = line.split("=");
-
-                if (line.startsWith("#"))
-                    continue;
-
-                if (split.length < 2)
-                    continue;
-
-                if (line.startsWith("mcsl-key=")) {
-                    this.properties.put("mcsl-key", split[1].trim().toString());
-                }
-
-                if (line.startsWith("update-interval=")) {
-                    this.properties.put("update-interval", split[1].trim().toString());
-                }
-            }
-        }
-        catch (Exception e) {
-            log.info("[MCSL] Error (MCSL.properties) - " + e);
-            return false;
-        }
-        finally {
-            msclScanner.close();
-        }
-
-        return true;
-    }
-
-    public boolean setupProperties(File folder) {
-        folder.mkdirs();
-
-        try {
-            this.config.createNewFile();
-
-            FileWriter fstream = new FileWriter(this.config);
-            BufferedWriter out = new BufferedWriter(fstream);
-            out.write("# Unique API-Key for your server from the MCSL Website.\n");
-            out.write("mcsl-key=\n");
-            out.write("# How long of an interval between each MCSL Update.\n");
-            out.write("# Default = 30 Seconds, anything lower than 10 Seconds is ignored.\n");
-            out.write("update-interval=30\n");
-
-            out.close();
-            fstream.close();
-
-            log.info("[MCSL] Default properties file created, please edit the file to include your API-Key to allow MCSL to Enable");
-        }
-        catch (IOException e) {
-            log.log(Level.SEVERE, "[MCSL] Error - " + e);
-            return false;
-        }
-
-        return true;
-    }
-
-    public void startData() {
-        try {
-            String data = "";
-
-            data += URLEncoder.encode("key", "UTF-8") + "=" + URLEncoder.encode(this.properties.get("mcsl-key"), "UTF-8") + "&";
-
-            for (String key : this.properties.keySet()) {
-                String value = this.properties.get(key);
-                if ((!key.equalsIgnoreCase("mcsl-key")) && (!key.equalsIgnoreCase("update-interval"))) {
-                    data += URLEncoder.encode(key, "UTF-8") + "=" + URLEncoder.encode(value, "UTF-8") + "&";
-                }
-            }
-
-            data += "plugins=";
-
-            String plugins = "";
-
-            for (int pos = 0; pos < getServer().getPluginManager().getPlugins().length; pos++) {
-                plugins += getServer().getPluginManager().getPlugins()[pos].getDescription().getName() + ", ";
-            }
-
-            data += URLEncoder.encode(plugins, "UTF-8");
-
-            data = data.substring(0, data.length() - 2);
-
-            updater.Update(data);
-        }
-        catch (UnsupportedEncodingException e) {
-            log.info("[MCSL] Error - " + e);
-        }
-    }
+	@SuppressWarnings("LoggerStringConcat")
+	public void onDisable()
+	{
+		logger.info(getDescription().getName() + " version " + getDescription().getVersion() + " disabled.");
+	}
 }
--- src/com/bukkit/wedtm/mcsl/MCSLPlayerListener.java
+++ src/com/bukkit/wedtm/mcsl/MCSLPlayerListener.java
@@ -1,39 +1,193 @@
 package com.bukkit.wedtm.mcsl;
 
-import java.util.logging.Logger;
+import java.io.*;
+import java.lang.reflect.*;
+import java.net.*;
+import java.util.logging.*;
+import net.minecraft.server.*;
+import org.bukkit.*;
+import org.bukkit.craftbukkit.*;
+import org.bukkit.entity.*;
+import org.bukkit.event.player.*;
 
-import org.bukkit.event.player.PlayerEvent;
-import org.bukkit.event.player.PlayerListener;
-import org.bukkit.event.player.PlayerLoginEvent;
 
-/**
- * Handle events for all Player related events
- * 
- * @author WedTM
- */
-public class MCSLPlayerListener extends PlayerListener {
-    private final MCSL   plugin;
+public class McslPlayerListener extends PlayerListener
+{
+	private final static Logger logger = Logger.getLogger("Minecraft");
+	private boolean running = true;
+	private final Server server;
+	private int maxPlayers = 0;
+	private String mcslKey = "";
+	private Thread thread;
 
-    private final Logger log = Logger.getLogger("Minecraft");
+	@SuppressWarnings("CallToThreadStartDuringObjectConstruction")
+	public McslPlayerListener(Mcsl parent)
+	{
+		this.server = parent.getServer();
 
-    public MCSLPlayerListener(MCSL instance) {
-        plugin = instance;
-    }
+		// Get the data from the server.properties file as the server sees it, rather than reading it manually
+		try
+		{
+			// Use reflection to get a private MinecraftServer instance
+			ServerConfigurationManager mgr = ((CraftServer)server).getHandle();
+			Field c = ServerConfigurationManager.class.getDeclaredField("c");
+			c.setAccessible(true); // Our workaround to turn private into public
+			MinecraftServer mc = (MinecraftServer)c.get(mgr);
 
-    @Override
-    public void onPlayerLogin(PlayerLoginEvent event) {
-        String pName = event.getPlayer().getName();
-        log.info("[MCSL] " + pName + " logged in.");
-        plugin.updater.UpdatePlayerCount();
-    }
-    
-    @Override
-    public void on
+			// Read values from configuration
+			this.maxPlayers = mc.d.a("max-players", 20);
+			this.mcslKey = mc.d.a("mcsl-key", "");
+		}
+		catch (Exception ex)
+		{
+			// Disable the plugin
+			logger.log(Level.WARNING, "Error encountered while initializing MCServerlist plugin.", ex);
+			parent.getPluginLoader().disablePlugin(parent);
+			return;
+		}
 
-    @Override
-    public void onPlayerQuit(PlayerEvent event) {
-        String pName = event.getPlayer().getName();
-        log.info("[MCSL] " + pName + " logged out.");
-        plugin.updater.UpdatePlayerCount();
-    }
+		// Check for invalid MCSL key
+		if (this.mcslKey == null || "".equals(this.mcslKey))
+		{
+			logger.warning("Invalid or nonexistent MCServerlist key in server properties.");
+			parent.getPluginLoader().disablePlugin(parent);
+			return;
+		}
+
+		// Run Update on a set interval of 1 minute with an initial delay of 10 seconds
+		thread = new Thread(new UpdateRunnable());
+		thread.setDaemon(true);
+		thread.start();
+	}
+
+	@Override
+	@SuppressWarnings("FinalizeDeclaration")
+	protected void finalize() throws Throwable
+	{
+		// Stop the timer
+		if (thread != null && thread.isAlive())
+		{
+			running = false;
+			thread.join();
+		}
+
+		super.finalize();
+	}
+
+	@Override
+	public void onPlayerJoin(PlayerEvent event)
+	{
+		forceUpdate();
+	}
+
+	@Override
+	public void onPlayerQuit(PlayerEvent event)
+	{
+		forceUpdate();
+	}
+
+	public void forceUpdate()
+	{
+		if (thread != null && thread.isAlive()) thread.interrupt();
+	}
+
+
+	private class UpdateRunnable implements Runnable
+	{
+		@SuppressWarnings("SleepWhileHoldingLock")
+		public void run()
+		{
+			do
+			{
+				update();
+				try
+				{
+					Thread.sleep(60000);
+				}
+				catch (InterruptedException ex)
+				{
+					logger.info("Forcing MCServerlist update.");
+				}
+			}
+			while (running);
+		}
+
+		@SuppressWarnings("CallToThreadDumpStack")
+		public void update()
+		{
+			// Check that we aren't receiving an event inappropriately
+			if (mcslKey == null || mcslKey.equals("")) return;
+			// Compile a comma-space-delimted list of players
+			Player[] players = server.getOnlinePlayers();
+			StringBuilder list = new StringBuilder();
+			if (players.length > 0)
+			{
+				for (int i = 0; i < players.length; i++)
+				{
+					if (i > 0) list.append(", ");
+					list.append(players[i].getName());
+				}
+			}
+
+			try
+			{
+				// Compile POST data
+				StringBuilder data = new StringBuilder();
+				data.append("key=");
+				data.append(URLEncoder.encode(mcslKey, "UTF-8"));
+				data.append("&player_count=");
+				data.append(Integer.toString(players.length));
+				data.append("&max_players=");
+				data.append(Integer.toString(maxPlayers));
+				data.append("&player_list=");
+				data.append(URLEncoder.encode(list.toString(), "UTF-8"));
+
+				OutputStreamWriter tx = null;
+				BufferedReader rx = null;
+				try
+				{
+					// Send POST request
+					URL url = new URL("http://mcserverlist.net/api/update");
+					// Swap line for testing purposes
+					//URL url = new URL("http://localhost/mcsl.php");
+					HttpURLConnection http = (HttpURLConnection)url.openConnection();
+					http.setRequestMethod("POST");
+					http.setUseCaches(false);
+					http.setConnectTimeout(1000);
+					http.setAllowUserInteraction(false);
+					http.setInstanceFollowRedirects(true);
+					http.setRequestProperty("User-Agent", "Java;Mcsl");
+					http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+					http.setRequestProperty("X-Mcsl-Key", mcslKey.replaceAll("[^a-zA-Z0-9]", ""));
+					http.setRequestProperty("X-Minecraft-Name", URLEncoder.encode(server.getName(), "UTF-8"));
+					http.setRequestProperty("X-Minecraft-Version", server.getVersion());
+					http.setDoInput(true);
+					http.setDoOutput(true);
+					tx = new OutputStreamWriter(http.getOutputStream());
+					tx.write(data.toString());
+					tx.flush();
+
+					// Get the HTTP response
+					rx = new BufferedReader(new InputStreamReader(http.getInputStream()));
+					for (String l = ""; rx.ready(); l = rx.readLine())
+					{
+						if ("".equals(l)) continue;
+						else if (l.startsWith("i:")) logger.info(l.substring(2));
+						else if (l.startsWith("w:")) logger.warning(l.substring(2));
+						else System.out.println(l);
+					}
+				}
+				finally
+				{
+					if (tx != null) tx.close();
+					if (rx != null) rx.close();
+				}
+			}
+			catch (Exception ex)
+			{
+				logger.log(Level.WARNING, "Error communication with MCServerlist.", ex);
+				ex.printStackTrace();
+			}
+		}
+	}
 }
